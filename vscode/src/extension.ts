4df5be2b56fa531ebfcd2b7ea3491917645d2bc0
Karoliine Holter <karoliine.holter@hotmail.com>
2025-01-20 16:36:48 +0200

    Modify VSCode extension code

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -1,25 +1,30 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import {LanguageClient, LanguageClientOptions, ServerOptions} from 'vscode-languageclient/node';
 
 // This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "sv-comp-verifiers" is now active!');
+	let script = 'java';
+	let args = ['-jar', context.asAbsolutePath('sv-comp-vscode-0.0.1.jar')];
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('sv-comp-verifiers.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from sv-comp-verifiers!');
-	});
+	// Use this for communicating on stdio
+	let serverOptions: ServerOptions = {
+		run: {command: script, args: args},
+		debug: {command: script, args: args},
+	};
 
-	context.subscriptions.push(disposable);
+	let clientOptions: LanguageClientOptions = {
+		documentSelector: [{scheme: 'file', language: 'c'}],
+		synchronize: {
+			fileEvents: [vscode.workspace.createFileSystemWatcher('**/*.c')]
+		}
+	};
+
+	// Create the language client and start the client.
+	let lc: LanguageClient = new LanguageClient('sv-comp-verifiers', 'sv-comp-verifiers', serverOptions, clientOptions);
+	lc.start();
 }
 
 // This method is called when your extension is deactivated

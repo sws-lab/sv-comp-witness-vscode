6575fc7c34213519648f0f2094c5704edc0dd642
Karoliine Holter <karoliine.holter@hotmail.com>
2025-03-05 21:47:55 +0200

    Convert Main to kotlin

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,28 @@
+import fmweckserver.FmWeckClient
+import fmweckserver.FmWeckServer
+import lsp.WitnessLanguageServer
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import witnesses.AnalysisManager
+
+object Main {
+    // TODO: hardcoded port
+    private const val PORT = 50051
+
+    private val log: Logger = LogManager.getLogger(Main::class.java)
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        val fmWeckClient = FmWeckClient("localhost", PORT)
+        val fmWeckServer = FmWeckServer()
+        fmWeckServer.startFmWeckServer(PORT)
+        val analysisManager = AnalysisManager(fmWeckClient)
+        createLanguageServer(analysisManager)
+    }
+
+    private fun createLanguageServer(analysisManager: AnalysisManager) {
+        val witnessLanguageServer = WitnessLanguageServer(analysisManager)
+        witnessLanguageServer.launchOnStream(System.`in`, System.out)
+        log.info("Language server launched.")
+    }
+}

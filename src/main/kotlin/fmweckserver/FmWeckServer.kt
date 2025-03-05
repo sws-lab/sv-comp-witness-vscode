ea1056b017c064318062ae16670ecda6e6fc8cc0
Karoliine Holter <karoliine.holter@hotmail.com>
2025-03-05 13:04:38 +0200

    Convert FmWeckServer to kotlin

---
+++ src/main/kotlin/fmweckserver/FmWeckServer.kt
@@ -0,0 +1,30 @@
+package fmweckserver
+
+import org.apache.logging.log4j.LogManager
+import org.apache.logging.log4j.Logger
+import java.io.IOException
+
+class FmWeckServer {
+    private val log: Logger = LogManager.getLogger(FmWeckServer::class.java)
+
+    // TODO: implement proper shut-down
+    fun startFmWeckServer(port: Int) {
+        val processBuilder = ProcessBuilder(
+            "fm-weck",
+            "server",
+            "--port",
+            port.toString(),
+            "--listen",
+            "localhost"
+        )
+        processBuilder.redirectErrorStream(true)
+        try {
+            processBuilder.start()
+            log.info("fm-weck server started on port $port")
+            // TODO: redirect server output to IDE output-log
+        } catch (e: IOException) {
+            log.error("Failed to start fm-weck server", e)
+            TODO("proper error handling")
+        }
+    }
+}

1a724bb9426ae7eb36374ee4a90ee8b69f674e1b
Karoliine Holter <karoliine.holter@hotmail.com>
2025-03-26 19:29:36 +0200

    Add tests for creating AST from variables and constants

---
+++ src/test/kotlin/c/CInvariantAstTest.kt
@@ -0,0 +1,29 @@
+package c
+
+import c.invariantAST.Node
+import c.invariantAST.Node.Companion.constant
+import c.invariantAST.Node.Companion.variable
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+class CInvariantAstTest {
+
+    private val invariant = "(((i == 0) && (j == 0)) || ((i == 0) && (1 <= j)))";
+
+    @Test
+    fun test_variable_constant() {
+        legal("x", variable("x"))
+        legal("xyz", variable("xyz"))
+        legal("x1", variable("x1"))
+        legal("1", constant("1"))
+        legal("0.0", constant("0.0"))
+        legal("1L", constant("1L"))
+        legal("1u", constant("1u"))
+        legal("'c'", constant("'c'"))
+    }
+
+    private fun legal(input: String, expectedAst: Node) {
+        val actualAst = CInvariantAst.createAst(input)
+        assertEquals(expectedAst, actualAst)
+    }
+}

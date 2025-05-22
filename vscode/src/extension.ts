046779ed31d714f7acc069210683e81dd058038e
Karoliine Holter <karoliine.holter@hotmail.com>
2025-05-22 12:16:31 +0300

    Reformat extension.ts

--- vscode/src/extension.ts
+++ vscode/src/extension.ts
@@ -7,32 +7,32 @@ import {InvariantInfoProvider} from "./invariantInfoProvider";
 
 // This method is called when your extension is activated
 export function activate(context: vscode.ExtensionContext) {
-	let script = 'java';
-	let args = ['-jar', context.asAbsolutePath('sv-comp-witness-vscode-0.0.1.jar')];
+    let script = 'java';
+    let args = ['-jar', context.asAbsolutePath('sv-comp-witness-vscode-0.0.1.jar')];
 
-	// Use this for communicating on stdio
-	let serverOptions: ServerOptions = {
-		run: {command: script, args: args},
-		debug: {command: script, args: args},
-	};
+    // Use this for communicating on stdio
+    let serverOptions: ServerOptions = {
+        run: {command: script, args: args},
+        debug: {command: script, args: args},
+    };
 
-	let clientOptions: LanguageClientOptions = {
-		documentSelector: [{scheme: 'file', language: 'c'}],
-		synchronize: {
-			fileEvents: [vscode.workspace.createFileSystemWatcher('**/*.c')]
-		}
-	};
+    let clientOptions: LanguageClientOptions = {
+        documentSelector: [{scheme: 'file', language: 'c'}],
+        synchronize: {
+            fileEvents: [vscode.workspace.createFileSystemWatcher('**/*.c')]
+        }
+    };
 
-	// Create the language client and start the client.
-	let lc = new LanguageClient('sv-comp-verifiers', 'sv-comp-verifiers', serverOptions, clientOptions);
-	context.subscriptions.push(lc);
+    // Create the language client and start the client.
+    let lc = new LanguageClient('sv-comp-verifiers', 'sv-comp-verifiers', serverOptions, clientOptions);
+    context.subscriptions.push(lc);
 
-	// Wait until the client is ready before registering commands
-	lc.start().then(() => {
-		// Register a WebView view
-		context.subscriptions.push(
-			vscode.window.registerWebviewViewProvider('witnessPanel', new WitnessViewProvider(context, lc))
-		);
+    // Wait until the client is ready before registering commands
+    lc.start().then(() => {
+        // Register a WebView view
+        context.subscriptions.push(
+            vscode.window.registerWebviewViewProvider('witnessPanel', new WitnessViewProvider(context, lc))
+        );
 
         const invariantProvider = new InvariantInfoProvider();
         context.subscriptions.push(
@@ -41,9 +41,9 @@ export function activate(context: vscode.ExtensionContext) {
         vscode.commands.registerCommand('showInvariantInfo', (data: string) => {
             invariantProvider.setData(data);
         });
-	}).catch(error => {
-		console.error("Error starting language client:", error);
-	});
+    }).catch(error => {
+        console.error("Error starting language client:", error);
+    });
 
 
 }
